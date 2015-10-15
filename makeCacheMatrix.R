f33f0063d38db57a324de9f0e987e571d842da84
sophiadai <sophiadai@users.noreply.github.com>
2015-10-14 23:32:44 -0700

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -12,3 +12,15 @@ makeCacheMatrix <- function(x = matrix()) {
          getMatrixInverse = getMatrixInverse) 
 }
 
+cacheSolve <- function(x, ...) { 
+    m <- x$getMatrixInverse() 
+    if(!is.null(m)) { 
+        message("getting cached matrix") 
+        return(m) 
+    } 
+    message("inverse matrix is not in memory, it will be computed now...")
+    data <- x$get() 
+    m <- solve(data, ...) 
+    x$setMatrixInverse(m) 
+    m 
+}
